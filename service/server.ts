cdaac1a3417e2c227f5fd01f370ad333be50ef43
Ege Aksoz <egeaksoz@gmail.com>
2024-12-25 17:10:36 +0000

    add mongodb connection

--- service/server.ts
+++ service/server.ts
@@ -1,9 +1,15 @@
-import { Hono } from "@hono/hono"
+import { Hono } from "@hono/hono";
+import { homeLibrary } from "./db/db.ts";
 
-const app = new Hono()
+const app = new Hono();
 
 app.get("/", (c) => {
-	return c.text("New deno project")
-})
+	return c.text("New deno project");
+});
+
+app.get("/books", async (c) => {
+	const books = await homeLibrary.find().toArray();
+	return c.json(books);
+});
 
 Deno.serve(app.fetch);
