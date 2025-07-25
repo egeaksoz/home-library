d8fa55f9d6385839432925a4d5d27c3098076ada
Ege Aksoz <egeaksoz@gmail.com>
2025-07-25 14:48:42 +0100

    add get and add book endpoints

--- service/app/main.py
+++ service/app/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Book(BaseModel):
+    title: str
+    author: str
+    language: str
+    genre: str
+    cover: str
+    read: bool
+    description: str
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
+@app.get("/books/{book_name}")
+def get_book(book_name: str):
+    return {"message": f"Here is your book: {book_name}"}
+    
 @app.get("/books")
 def get_books():
     return {"title": "My Name is Red", "author": "Orhan Pamuk", "language": "Turkish"}
+
+@app.post("/books")
+def add_book(book: Book):
+    return book
