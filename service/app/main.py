6f1505d0532efc9c5aa4416dc44f4edeea2b6273
Ege Aksoz <egeaksoz@gmail.com>
2025-08-31 18:59:28 +0100

    add versioning to the api

--- service/app/main.py
+++ service/app/main.py
@@ -12,14 +12,14 @@ class Book(BaseModel):
 
 app = FastAPI()
 
-@app.get("/books/{book_name}")
+@app.get("/v1/books/{book_name}")
 def get_book(book_name: str):
     return {"message": f"Here is your book: {book_name}"}
     
-@app.get("/books")
+@app.get("/v1/books")
 def get_books():
     return {"title": "My Name is Red", "author": "Orhan Pamuk", "language": "Turkish"}
 
-@app.post("/books")
+@app.post("/v1/books")
 def add_book(book: Book):
     return book
