90200f06fba49fbe11f9ff8c62fe6a48c3e1d814
Ege Aksoz <egeaksoz@gmail.com>
2025-09-27 12:31:42 +0100

    remove v1 suffix

--- service/app/main.py
+++ service/app/main.py
@@ -22,7 +22,7 @@ create_db()
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
-@app.get("/v1/libraries")
+@app.get("/libraries")
 def read_libraries(
     session: SessionDep,
     offset: int = 0,
@@ -30,18 +30,18 @@ def read_libraries(
     libraries = session.exec(select(Library).offset(offset).limit(limit)).all()
     return libraries
 
-@app.post("/v1/libraries")
+@app.post("/libraries")
 def add_library(library: Library):
     return library
 
-@app.get("/v1/books/{book_name}")
+@app.get("/books/{book_name}")
 def get_book(book_name: str):
     return {"message": f"Here is your book: {book_name}"}
     
-@app.get("/v1/books")
+@app.get("/books")
 def get_books():
     return {"title": "My Name is Red", "author": "Orhan Pamuk", "language": "Turkish"}
 
-@app.post("/v1/books")
+@app.post("/books")
 def add_book(book: Book):
     return book
