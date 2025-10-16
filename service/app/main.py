d7d5c266ec623cee166e52a4a30d185aab889d9c
Ege Aksoz <egeaksoz@gmail.com>
2025-10-16 04:07:21 +0100

    add book router

--- service/app/main.py
+++ service/app/main.py
@@ -1,31 +1,10 @@
-
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 from app.database import create_db
 from app.routes.library import library_router
-
-class Book(BaseModel):
-    title: str
-    author: str
-    language: str
-    genre: str
-    cover: str
-    read: bool
-    description: str
+from app.routes.book import book_router
 
 app = FastAPI()
 app.include_router(library_router)
+app.include_router(book_router)
 create_db()
-
-@app.get("/books/{book_name}")
-def get_book(book_name: str):
-    return {"message": f"Here is your book: {book_name}"}
-    
-@app.get("/books")
-def get_books():
-    return {"title": "My Name is Red", "author": "Orhan Pamuk", "language": "Turkish"}
-
-@app.post("/books")
-def add_book(book: Book):
-    return book
--- service/app/models.py
+++ service/app/models.py
@@ -4,3 +4,12 @@ class Library(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
     number_of_books: int | None
+
+class Book(SQLModel):
+    title: str
+    author: str
+    language: str
+    genre: str
+    cover: str
+    read: bool
+    description: str
