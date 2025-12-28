830a2fb412aed29250a03899a0695a78ced2a8c1
Ege Aksoz <egeaksoz@gmail.com>
2025-12-28 17:32:01 +0000

    add temporary mobile nav

--- ui/src/components/Header/Header.tsx
+++ ui/src/components/Header/Header.tsx
@@ -1,11 +1,17 @@
 import "./Header.css";
-import { type FC } from "react";
+import { type FC, useState } from "react";
+import { X, Menu } from "lucide-react";
 
 const Header: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header>
+      <button onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <X /> : <Menu />}
+      </button>
       <nav>
-        <ul>
+        <ul className={isOpen ? "mobile-open" : "mobile-close"}>
           <li>
             <a href="/">
               <span>Home</span>
