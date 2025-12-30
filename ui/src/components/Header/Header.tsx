e101009df0a672c84d9f7c4af5067335108395d1
Ege Aksoz <egeaksoz@gmail.com>
2025-12-30 20:50:35 +0000

    add hamburger menu to header nav

--- ui/src/components/Header/Header.tsx
+++ ui/src/components/Header/Header.tsx
@@ -1,27 +1,35 @@
 import "./Header.css";
-import { type FC, useState } from "react";
-import { X, Menu } from "lucide-react";
+import { useRef, type FC, useState } from "react";
+import { Menu, X, House, Library } from "lucide-react";
 
 const Header: FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const navRef = useRef<HTMLButtonElement>(null);
+
+  const toggleSideMenu = (): void => {
+    navRef.current!.classList.toggle("show");
+    setIsOpen(!isOpen);
+  };
 
   return (
     <header>
-      <button onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? <X /> : <Menu />}
-      </button>
-      <nav>
-        <ul className={isOpen ? "mobile-open" : "mobile-close"}>
+      <button onClick={toggleSideMenu}>{isOpen ? <X /> : <Menu />}</button>
+      <nav ref={navRef}>
+        <ul>
           <li>
             <a href="/">
+              <House />
               <span>Home</span>
             </a>
           </li>
           <li>
-            <a href="/libraries">Libraries</a>
+            <a href="/libraries">
+              <Library /> <span>Libraries</span>
+            </a>
           </li>
         </ul>
       </nav>
+      <div id="overlay" onClick={toggleSideMenu} aria-hidden="true"></div>
     </header>
   );
 };
